34565ab9c8496b70abd703921502ebb9fa1f811f
theooprea <opreatheodor54@gmail.com>
2020-09-29 12:25:10 +0300

    dishRouter

---
+++ node-express/index.js
@@ -0,0 +1,23 @@
+const express = require('express'),
+     http = require('http');
+
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+const dishRouter = require('./routes/dishRouter');
+
+app.use('/dishes', dishRouter);
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
