c0d615248e2f52867cbedbd9821d0c26303fe746
theooprea <opreatheodor54@gmail.com>
2020-09-30 13:34:51 +0300

    mongoose

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -3,34 +3,42 @@ const assert = require('assert');
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
-
-MongoClient.connect(url, (err, client) => {
-
-    assert.equal(err,null);
+const dboper = require('./operations');
+MongoClient.connect(url).then((client) => {
 
     console.log('Connected correctly to server');
-
     const db = client.db(dbname);
-    const collection = db.collection("dishes");
-    collection.insertOne({"name": "Uthappizza", "description": "test"},
-    (err, result) => {
-        assert.equal(err,null);
 
-        console.log("After Insert:\n");
-        console.log(result.ops);
+    dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
+        "dishes")
+        .then((result) => {
+            console.log("Insert Document:\n", result.ops);
 
-        collection.find({}).toArray((err, docs) => {
-            assert.equal(err,null);
-            
-            console.log("Found:\n");
-            console.log(docs);
+            return dboper.findDocuments(db, "dishes");
+        })
+        .then((docs) => {
+            console.log("Found Documents:\n", docs);
 
-            db.dropCollection("dishes", (err, result) => {
-                assert.equal(err,null);
+            return dboper.updateDocument(db, { name: "Vadonut" },
+                    { description: "Updated Test" }, "dishes");
 
-                client.close();
-            });
-        });
-    });
+        })
+        .then((result) => {
+            console.log("Updated Document:\n", result.result);
 
-});
+            return dboper.findDocuments(db, "dishes");
+        })
+        .then((docs) => {
+            console.log("Found Updated Documents:\n", docs);
+                            
+            return db.dropCollection("dishes");
+        })
+        .then((result) => {
+            console.log("Dropped Collection: ", result);
+
+            return client.close();
+        })
+        .catch((err) => console.log(err));
+
+})
+.catch((err) => console.log(err));
